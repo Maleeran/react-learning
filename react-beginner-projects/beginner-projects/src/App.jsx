4c9845e712879dababcd1543b0c032fa56e5c843
Liangbo Gao <15803871761@163.com>
2026-02-06 20:24:36 +0800

    2.6save

--- react-beginner-projects/beginner-projects/src/App.jsx
+++ react-beginner-projects/beginner-projects/src/App.jsx
@@ -2,6 +2,12 @@ import Counter from "./Components/counter";
 import Todo from "./Components/Todo";
 import Meals from "./Components/Meals";
 import Calculator from "./Components/Calculator";
+import ToggleColor from "./Components/ToggleColor";
+import HiddenSearchBar from "./Components/HiddenSearchBar";
+import Testmonials from "./Components/Testmonials";
+import { accordionData } from "./utils/content";
+import Accordion from "./Components/Accordion";
+import Form from "./Components/Form";
 
 const App = () => {
   return (
@@ -9,7 +15,16 @@ const App = () => {
       {/* <Counter /> */}
       {/* <Todo/> */}
       {/* <Meals /> */}
-      <Calculator />
+      {/* <Calculator /> */}
+      {/* <ToggleColor /> */}
+      {/* <HiddenSearchBar /> */}
+      {/* <Testmonials /> */}
+      {/* <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div> */}
+      <Form />
     </div>
   );
 };
---
+++ react-beginner-projects/beginner-projects/src/Components/Testmonials.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "../style.css";
+
+const Testmonials = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const testimonials = [
+    {
+      quote: "This is the best product I've ever used!",
+      author: "Jane Doe",
+    },
+    {
+      quote: "I highly recommend this product to everyone!",
+      author: "John Smith",
+    },
+    {
+      quote: "This product has completely changed my life!",
+      author: "Bob Johnson",
+    },
+  ];
+
+  const handlePrevClick = () => {
+    if (currentIndex !== 0) setCurrentIndex(currentIndex - 1);
+    else setCurrentIndex(testimonials.length - 1);
+  };
+
+  const handleNextClick = () => {
+    if (currentIndex !== testimonials.length - 1)
+      setCurrentIndex(currentIndex + 1);
+    else setCurrentIndex(0);
+  };
+  return (
+    <div className="testimonials">
+      <div className="testimonials-quote">
+        {testimonials[currentIndex].quote}
+      </div>
+      <div className="testimonials-author">
+        - {testimonials[currentIndex].author}
+      </div>
+      <div className="testimonials-nav">
+        <button onClick={handlePrevClick}>Prev</button>
+        <button onClick={handleNextClick}>Next</button>
+      </div>
+    </div>
+  );
+};
+export default Testmonials;
